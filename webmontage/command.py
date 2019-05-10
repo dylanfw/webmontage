27a5cb02f761a6b153c846f2ac3092a76eccfd0f
Dylan Williams <dylan@dylanfw.com>
2019-05-10 17:32:02 -0500

    Complete display of montage, adding extra arguments for explicit file tracking, and browser size

--- webmontage/command.py
+++ webmontage/command.py
@@ -5,9 +5,9 @@ import time
 import tempfile
 
 import cv2
-import imutils
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+import numpy as np
 from selenium import webdriver
 
 
@@ -25,7 +25,38 @@ def get_args():
         help="The absolute path to the root of the git repository for the web page",
     )
     parser.add_argument(
-        "index", metavar="index", help="The index file relative to the root of the repo"
+        "index",
+        metavar="index",
+        help="The index file relative to the root of the repo, i.e. 'index.html'",
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="If given, only commits that affect this file will be considered, i.e. 'main.css'",
+    )
+    parser.add_argument(
+        "--browser-width",
+        type=int,
+        default=1440,
+        help="Width of the browser window to load the web page in",
+    )
+    parser.add_argument(
+        "--browser-height",
+        type=int,
+        default=1800,
+        help="Height of the browser window to load the web page in",
+    )
+    parser.add_argument(
+        "--video-width",
+        type=int,
+        default=640,
+        help="Width of the slideshow window to display the montage in",
+    )
+    parser.add_argument(
+        "--video-height",
+        type=int,
+        default=800,
+        help="Height of the slideshow window to display the montage in",
     )
     return parser.parse_args()
 
@@ -34,7 +65,7 @@ def dhash(image, hash_size=8):
     # https://www.pyimagesearch.com/2017/11/27/image-hashing-opencv-python/
     resized = cv2.resize(image, (hash_size + 1, hash_size))
     diff = resized[:, 1:] > resized[:, :-1]
-    return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+    return sum(2 ** i for (i, v) in enumerate(diff.flatten()) if v)
 
 
 class Git:
@@ -45,15 +76,19 @@ class Git:
         except (InvalidGitRepositoryError, NoSuchPathError) as e:
             raise WebMontageError(f"No git repository found at {path}")
 
-    def file_history(self, filename):
+    def history(self, filename=None):
         try:
-            commits = self.client.log(filename, pretty='format:"%h"', follow=True)
-            commits = commits.replace('"', "").split("\n")
+            if filename:
+                commits = self.client.log(filename, pretty='format:"%h"', follow=True)
+            else:
+                commits = self.client.log(pretty='format:"%h"')
         except GitCommandError as e:
             raise WebMontageError(
                 f'Error occurred finding commits. Make sure {self.path}/{filename} exists and has a commit history: > git log --pretty=format:"%h" --follow -- {filename}'
             )
-        return reversed(commits)
+        commits = commits.replace('"', "").split("\n")
+        self.commits = reversed(commits)
+        return self.commits
 
     def checkout(self, commit):
         self.client.checkout(commit)
@@ -68,13 +103,14 @@ class Git:
 
 
 class Browser:
-    def __init__(self):
+    def __init__(self, width, height):
         try:
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument("headless")
             self.driver = webdriver.Chrome(
                 "chromedriver", chrome_options=chrome_options
             )
+            self.driver.set_window_size(width, height)
         except Exception:
             raise WebMontageError(
                 'Unable to create headless Chrome instance. Make sure "chromedriver" is installed and is in the PATH'
@@ -90,22 +126,53 @@ class Browser:
 def main():
     args = get_args()
     with tempfile.TemporaryDirectory() as tmp_dir:
-        with Git(args.repo) as git, Browser() as browser:
-            history = git.file_history(args.index)
+        with Git(args.repo) as git, Browser(
+            args.browser_width, args.browser_height
+        ) as browser:
+            history = list(git.history(filename=args.file))
+            print(f"Searching through {len(history)} commits...")
             browser.get(f"file:///{args.repo}/{args.index}")
             images = {}
+            max_width, max_height = 0, 0
             for i, commit in enumerate(history):
                 git.checkout(commit)
                 browser.refresh()
                 filename = f"{tmp_dir}/{i}.png"
-                body = browser.find_element_by_tag_name("body")
-                body.screenshot(filename)
+                browser.save_screenshot(filename)
 
                 # Many commits won't have any visual difference. We only want those images that generate unique hashes
                 image = cv2.imread(filename)
+                image = cv2.resize(image, (args.video_width, args.video_height))
                 image_hash = dhash(image)
                 images[image_hash] = images.get(image_hash, image)
 
-    for image in images.values():
-        cv2.imshow("Image", image)
-        time.sleep(5)
+    print(f"Found {len(images)} visually distinct commits")
+    print("Preparing slideshow...")
+    previous_image = np.full((args.video_height, args.video_width, 3), 255, np.uint8)
+    cv2.namedWindow("WebMontage")
+    cv2.moveWindow("WebMontage", 0, 0)
+    cv2.imshow(
+        "WebMontage",
+        cv2.putText(
+            previous_image,
+            "Press any key to begin...",
+            (50, 50),
+            cv2.FONT_HERSHEY_TRIPLEX,
+            1,
+            2,
+        ),
+    )
+    cv2.waitKey(0)
+    for i, image in enumerate(images.values()):
+        print(f"Displaying version {i+1}/{len(images)}")
+        for alpha in range(1, 11):
+            alpha /= 10.0
+            cv2.imshow(
+                "WebMontage",
+                cv2.addWeighted(image, alpha, previous_image, 1 - alpha, 0.0),
+            )
+            cv2.waitKey(100)
+        cv2.waitKey(3000)
+        previous_image = image
+    cv2.waitKey(3000)
+    cv2.destroyAllWindows()
